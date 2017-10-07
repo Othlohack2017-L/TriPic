d339945f7e82fb34baa5319aad7e982ce04cbdd2
hawkde <akasatana309week@gmail.com>
2017-10-08 00:53:30 +0900

    カメラ(デバッグ中)

--- app/src/main/java/com/example/okano/trippic/MainActivity.kt
+++ app/src/main/java/com/example/okano/trippic/MainActivity.kt
@@ -1,21 +1,28 @@
 package com.example.okano.trippic
 
 
+import android.Manifest
+import android.app.Activity
 import android.app.Dialog
 import android.app.PendingIntent.getActivity
 import android.content.DialogInterface
 import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.database.sqlite.SQLiteDatabase
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.EditText
-import kotlinx.android.synthetic.main.activity_main.*
 import android.widget.TextView
+import android.widget.Toast
 import com.example.okano.trippic.DB.DBManager
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -23,6 +30,7 @@ class MainActivity : AppCompatActivity() {
     var eventname = "event"
     var db : SQLiteDatabase? = null
     var text : TextView? = null
+    var _imageUri: Uri? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -68,7 +76,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun launchCamera(view: View){
+        //クリック時の処理
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            val permissions = arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            ActivityCompat.requestPermissions(this, permissions, 2000)
+            return
+        }
+        val dateFormat = SimpleDateFormat("yyyyMMddHHmmss")  // （1）
+        val now = Date(System.currentTimeMillis())  // （1）
+        val nowStr = dateFormat.format(now)  // （1）
+        val fileName = "UseCameraActivityPhoto_$nowStr.jpg"  // （1）
 
+        val values = ContentValues()  // （2）//ファイル名設定
+        values.put(MediaStore.Images.Media.TITLE, fileName)  // （3）putでデータを格納
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")  // （4）
+
+        val resolver = contentResolver  // （5）
+        _imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)  // （6）新しいデータの格納先を確保しこれが一つ目、それを表すUriオブジェクトを生成してくれる
+
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)  // （7）
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, _imageUri)  // （8）
+        startActivityForResult(intent, 200)  // （9）
     }
 
     fun getLocation(){
@@ -84,6 +112,15 @@ class MainActivity : AppCompatActivity() {
         val minute = calendar.get(Calendar.MINUTE).toString()
         val second = calendar.get(Calendar.SECOND).toString()
         return year + "/" + month + "/" + day + " " + hour + ":" + minute + ":" + second
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {//戻ってきたときの処理　requestodeは識別子C resultCodeは結果を表す
+        if (requestCode == 200 && resultCode == Activity.RESULT_OK) {  // （1）
+            //Bitmap bitmap =  data.getParcelableExtra("data");  // （2）カメラアプリが撮影した画像を取得カメラアプリの画像は勝手にインテントに入ってる
+            //val ivCamera = findViewById(R.id.ivCamera) as ImageView  // その画像をImageViewに適用
+            //ivCamera.setImageURI(_imageUri)  // （3）
+            Toast.makeText(this@MainActivity, "+_imageUri+", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
