26d83cc769fa08c89c4402b65a29ea684e84e118
umadaikon <wjumayasai@gmail.com>
2017-10-07 23:45:22 +0900

    I want to merge

--- app/src/main/java/com/example/okano/trippic/MainActivity.kt
+++ app/src/main/java/com/example/okano/trippic/MainActivity.kt
@@ -1,9 +1,16 @@
 package com.example.okano.trippic
 
+import android.app.Dialog
+import android.app.PendingIntent.getActivity
+import android.content.DialogInterface
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +27,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun startLog(view: View){
-        
+        AlertDialog.Builder(this)
+                .setTitle("Trip name")
+                .setMessage("旅行の名前を入力してください")
+                .setPositiveButton("OK", object :DialogInterface.OnClickListener{
+                    override fun onClick(dialog: DialogInterface, which: Int){
+
+                    }
+                })
+                .setNegativeButton("CANCEL", null)
+                .show()
     }
 
     fun endLog(view: View){
