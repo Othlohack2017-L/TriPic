87886aac2682e141316b030accb1ac7ebb4a063a
houndflog <yib2of80@gmail.com>
2017-10-08 00:50:52 +0900

    GPSCoordinationの現在地を得る関数を実験的に実装

--- app/src/main/java/com/example/okano/trippic/gpsCoordination/GPSCoordination.kt
+++ app/src/main/java/com/example/okano/trippic/gpsCoordination/GPSCoordination.kt
@@ -2,12 +2,16 @@ package com.example.okano.trippic.gpsCoordination
 
 import android.location.Location
 import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.util.Log
+
 
 /**
  * Created by yusuke on 2017/10/07.
  */
 class GPSCoordination : LocationListener{
+    private val locationManager : LocationManager? = null
 
     override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
@@ -23,6 +27,7 @@ class GPSCoordination : LocationListener{
 
     override fun onLocationChanged(location: Location?) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Log.d("myTag",""+location?.longitude+":"+location?.latitude)
     }
 
 }
