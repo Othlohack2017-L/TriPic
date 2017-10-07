9543cc1b23e9b477f1cfdd918786942415dcd4b1
houndflog <yib2of80@gmail.com>
2017-10-07 23:58:54 +0900

    GPSCoordinationにLocationLisnerの追加

--- app/src/main/java/com/example/okano/trippic/gpsCoordination/GPSCoordination.kt
+++ app/src/main/java/com/example/okano/trippic/gpsCoordination/GPSCoordination.kt
@@ -1,7 +1,28 @@
 package com.example.okano.trippic.gpsCoordination
 
+import android.location.Location
+import android.location.LocationListener
+import android.os.Bundle
+
 /**
  * Created by yusuke on 2017/10/07.
  */
-class GPSCoordination {
+class GPSCoordination : LocationListener{
+
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderEnabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onProviderDisabled(provider: String?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onLocationChanged(location: Location?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
