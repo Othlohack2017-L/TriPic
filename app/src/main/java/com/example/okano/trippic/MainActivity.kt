188c4c6d870fa4c8a6bcb7350c9ca204aac2bf98
umadaikon <wjumayasai@gmail.com>
2017-10-07 18:37:18 +0900

    activity transition

--- app/src/main/java/com/example/okano/trippic/MainActivity.kt
+++ app/src/main/java/com/example/okano/trippic/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.okano.trippic
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun changeActivity(view: View){
+        val intent = Intent(this, MapsActivity::class.java)
+        startActivity(intent)
+    }
+
+    fun launchCamera(view: View){
+
+    }
+
+    fun startLog(view: View){
+
+    }
+
+    fun endLog(view: View){
+
+    }
+
+    fun getLocation(){
+
+    }
+
 }
