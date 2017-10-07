674f8b4cdec6ff9519a965c2e633f6972ae93b28
JUJU-surface <fds@fdasf>
2017-10-07 23:03:05 +0900

    DB動かせた

--- app/src/main/java/com/example/okano/trippic/MainActivity.kt
+++ app/src/main/java/com/example/okano/trippic/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.okano.trippic
 import android.app.Dialog
 import android.app.PendingIntent.getActivity
 import android.content.DialogInterface
+import android.content.ContentValues
 import android.content.Intent
+import android.database.sqlite.SQLiteDatabase
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
@@ -11,14 +13,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
+import android.widget.TextView
+import com.example.okano.trippic.DB.DBManager
 
 class MainActivity : AppCompatActivity() {
 
     var eventname = "event"
+    var db : SQLiteDatabase? = null
+    var text : TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val helper = DBManager(this)
+        db = helper.writableDatabase
+
+
     }
 
     fun changeActivity(view: View){
@@ -37,6 +48,15 @@ class MainActivity : AppCompatActivity() {
                 })
                 .setNegativeButton("CANCEL", null)
                 .show()
+
+        val insertValue = ContentValues()
+        insertValue.put("name",eventname)
+        db!!.insert("Trip",eventname, insertValue)
+        val arr: Array<String> = arrayOf("name")
+        val c = db!!.query("Trip",arr,null,null,null, null ,null)
+        if(c.moveToNext()){
+            
+        }
     }
 
     fun endLog(view: View){
