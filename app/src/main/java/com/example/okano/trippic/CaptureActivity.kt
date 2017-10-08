5c4329e833eea15876b6e4bcea9af05ca8888e00
umadaikon <wjumayasai@gmail.com>
2017-10-08 14:09:08 +0900

    output image Activity

---
+++ app/src/main/java/com/example/okano/trippic/CaptureActivity.kt
@@ -0,0 +1,12 @@
+package com.example.okano.trippic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CaptureActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_capture)
+    }
+}
